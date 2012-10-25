77e8a3bf1286c46c81e51ddd27bf17df0e74503e
Thiago <thiagoaslima@gmail.com>
2012-10-25 16:17:25 -0200

    JS citacao e title preenchidos automaticamente. CSS title hidden

--- inc/metaboxes/people/dados-pessoais.php
+++ inc/metaboxes/people/dados-pessoais.php
@@ -3,7 +3,7 @@
         <p>
             <?php $value = get_post_meta($post_id, '_lvm_people_lattes', true);  ?>
             <label id="label-lattes" class="required label mini" for="lvm_people_lattes">Currículo Lattes</label>
-            <input type="url" id="lattes" name="lvm_people_lattes" class="required xxlarge" value="<?php echo $value; ?>" placeholder="http://" required="required">
+            <input type="url" id="lvm_people_lattes" name="lvm_people_lattes" class="required xxlarge" value="<?php echo $value; ?>" placeholder="http://" required="required">
         </p>
 
     </div>
@@ -14,25 +14,25 @@
         <p>
             <?php $value = get_post_meta($post_id, '_lvm_people_nome', true);  ?>
             <label for="lvm_people_nome" class="required label mini">Nome</label>
-            <input type="text" required="required" class="required xxlarge" id="nome" name="lvm_people_nome" value="<?php echo $value; ?>" placeholder="Preencha com a primeira parte do nome.">
+            <input type="text" required="required" class="required xxlarge" id="lvm_people_nome" name="lvm_people_nome" value="<?php echo $value; ?>" placeholder="Preencha com a primeira parte do nome.">
             <br>
-            <small>Ex.: para 'João Carlos da Silva Souza', escreva 'João Carlos'.</small>
+            <small class="shift-mini">Ex.: para 'João Carlos da Silva Souza', escreva 'João Carlos'.</small>
         </p>
 
         <p>
             <?php $value = get_post_meta($post_id, '_lvm_people_sobrenome', true);  ?>
             <label for="lvm_people_sobrenome" class="required label mini">Sobrenome</label>
-            <input type="text" required="required" class="required xxlarge" id="nome" name="lvm_people_sobrenome" value="<?php echo $value; ?>"  placeholder="Preencha com a segunda parte do nome.">
+            <input type="text" required="required" class="required xxlarge" id="lvm_people_sobrenome" name="lvm_people_sobrenome" value="<?php echo $value; ?>"  placeholder="Preencha com a segunda parte do nome.">
             <br>
-            <small>Ex.: para 'João Carlos da Silva Souza', escreva 'Silva Souza'.</small>
+            <small class="shift-mini">Ex.: para 'João Carlos da Silva Souza', escreva 'Silva Souza'.</small>
         </p>
 
         <p>
             <?php $value = get_post_meta($post_id, '_lvm_people_citacao', true);  ?>
             <label for="lvm_people_citacao" class="required label mini">Citação</label>
-            <input type="text" id="nome" class="required xxlarge" name="lvm_people_citacao" value="<?php echo $value; ?>" placeholder="Preencha como deve constar nas bibliografias e citações.">
+            <input type="text" id="lvm_people_citacao" class="required xxlarge" name="lvm_people_citacao" value="<?php echo $value; ?>" placeholder="Preencha como deve constar nas bibliografias e citações.">
             <br>
-            <small>Ex.: para 'João Carlos da Silva Souza', escreva algo como 'Souza, J. C. S.'.</small>
+            <small class="shift-mini">Ex.: para 'João Carlos da Silva Souza', escreva algo como 'Souza, J. C. S.'.</small>
         </p>
 
     </div>
@@ -58,7 +58,7 @@
             ?>
 
             <label for="lvm_people_nasc" class="label mini">Nascimento</label>
-            <select name="lvm_people_dia" id="nasc_dia">
+            <select name="lvm_people_dia" id="lvm_people_nasc_dia">
             <?php 
                 $i=1; $ii=31;
                 while ( $i<=$ii ){
@@ -69,7 +69,7 @@
             ?>
             </select>
             /
-            <select name="lvm_people_mes" id="nasc_mes">
+            <select name="lvm_people_mes" id="lvm_people_nasc_mes">
             <?php
                 $meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro' ];
                 $i=0; $ii=11;
@@ -80,7 +80,7 @@
             ?>
             </select>   
             /
-            <select name="lvm_people_ano" id="nasc_ano">
+            <select name="lvm_people_ano" id="lvm_people_nasc_ano">
             <?php
                 $ii = date('Y'); $i = $ii - 100;
                 while( $i<= $ii ) {
--- js/lvm_people_form.js
+++ js/lvm_people_form.js
@@ -3,20 +3,20 @@
 	jQuery.fn.selectText = function(){
     var doc = document, 
 	    element = this[0], 
-	    range, selection
-    ;
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(element);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();        
-        range = document.createRange();
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-};
+	    range, selection;
+
+	    if (doc.body.createTextRange) {
+	        range = document.body.createTextRange();
+	        range.moveToElementText(element);
+	        range.select();
+	    } else if (window.getSelection) {
+	        selection = window.getSelection();        
+	        range = document.createRange();
+	        range.selectNodeContents(element);
+	        selection.removeAllRanges();
+	        selection.addRange(range);
+	    }
+	};
 
 
 	$(".lvm_options span").on("click", function (e) {
@@ -36,6 +36,65 @@
 		$termo.on("blur", function (e) {
 			$opt.css("display", "block");
 		})
+	})
+
+	$("#cpt_lvm_people_dados_pessoais").on("change", "#lvm_people_nome, #lvm_people_sobrenome", function (e) {
+		var $title = $("#title"),
+			$citacao = $("#lvm_people_citacao"),
+			$nome = $("#lvm_people_nome"),
+			nome = $.trim($nome.val()),
+			$sobre = $("#lvm_people_sobrenome"),
+			sobre = $.trim($sobre.val()),
+			title = nome + " " + sobre;
+
+		//title
+		$title.val(title)
+		$("#title-prompt-text").css("visibility", "hidden");
+
+		//citacao
+		var nomeSep = nome.split(" "),
+			sobreSep = sobre.split(" "),
+			cit = ( sobre != "" ) ? sobreSep.pop() + ", " : "";		
+
+		function iniciais (elem){
+
+			if( !elem.length ) return "";
+
+			var i = 0,
+				len = elem.length,
+				iniciais = "";
+			
+				console.log( len + " : " + elem)
+
+			while( i < len ){
+				var parte = elem[i];
+
+				if ( parte != "de" && parte != "da" && parte != "do" && parte != "das" && parte != "dos" && parte != "" && parte != " " ) {
+					var inicial = parte.charAt(0).toUpperCase() + ". ";
+					iniciais += inicial;
+				}
+
+				i++;
+			}
+
+			return iniciais;
+		}
+
+		// nome
+		nomeIn = iniciais(nomeSep); 
+
+		//sobrenome		
+		if ( sobreSep[sobreSep.length-1] == "de" || sobreSep[sobreSep.length-1] == "da" || sobreSep[sobreSep.length-1] == "do" || sobreSep[sobreSep.length-1] == "das" || sobreSep[sobreSep.length-1] == "dos" ) {
+			var prep = sobreSep.pop();
+			console.log("prep: " + prep);
+			cit = prep + " " + cit;
+		}
+		sobreIn = iniciais(sobreSep);
+
+		cit = cit + nomeIn + sobreIn;
+
+		$citacao.val(cit);
+			
 	})
 
 })(jQuery);
